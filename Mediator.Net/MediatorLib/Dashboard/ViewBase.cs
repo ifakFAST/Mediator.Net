3ccc2bedc85af09c23eb71ece1cbc84c105361f0
Christian Hübner <christian.huebner@ifak.eu>
2020-07-27 09:42:55 +0200

    Dashboard: ViewBase with default implementation of OnUiRequestAsync using reflection

--- Mediator.Net/MediatorLib/Dashboard/ViewBase.cs
+++ Mediator.Net/MediatorLib/Dashboard/ViewBase.cs
@@ -2,7 +2,11 @@
 // ifak e.V. licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using Ifak.Fast.Mediator;
+using Ifak.Fast.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Ifak.Fast.Mediator.Dashboard
@@ -19,11 +23,49 @@ namespace Ifak.Fast.Mediator.Dashboard
         protected Connection Connection { get; set; }
         protected ViewContext Context { get; set; }
 
+        protected virtual string RequestMethodNamePrefix => "UiReq_";
+
+        protected Dictionary<string, UiReqMethod> mapUiReqMethods = new Dictionary<string, UiReqMethod>();
+        public delegate Task<ReqResult> UIReqDelegate(object[] paras);
+
         public virtual Task OnInit(Connection connection, ViewContext context, DataValue config) {
             Connection = connection;
             Context = context;
             Config = config;
+
+            Type type = GetType();
+            string prefix = RequestMethodNamePrefix;
+            int N = prefix.Length;
+
+            MethodInfo[] methods =
+                    type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance)
+                    .Where(m => m.Name.StartsWith(prefix))
+                    .ToArray();
+
+            foreach (MethodInfo m in methods) {
+
+                ParameterInfo[] parameters = m.GetParameters();
+
+                UIReqDelegate theDelegate = (object[] args) => {
+                    return (Task<ReqResult>)m.Invoke(this, args);
+                };
+
+                UiReqPara[] uiReqParameters = parameters.Select(MakeParameter).ToArray();
+
+                string key = m.Name.Substring(N);
+                mapUiReqMethods[key] = new UiReqMethod(theDelegate, uiReqParameters);
+            }
+
             return Task.FromResult(true);
+        }
+
+        private static UiReqPara MakeParameter(ParameterInfo p) {
+            return new UiReqPara() {
+                Name = p.Name,
+                Type = p.ParameterType,
+                HasDefaultValue = p.HasDefaultValue,
+                DefaultValue = p.HasDefaultValue ? p.DefaultValue : null
+            };
         }
 
         public abstract Task OnActivate();
@@ -32,7 +74,27 @@ namespace Ifak.Fast.Mediator.Dashboard
 
         public virtual Task OnDestroy() => Task.FromResult(true);
 
-        public abstract Task<ReqResult> OnUiRequestAsync(string command, DataValue parameters);
+        public virtual async Task<ReqResult> OnUiRequestAsync(string command, DataValue parameters) {
+
+            if (mapUiReqMethods.ContainsKey(command)) {
+
+                UiReqMethod method = mapUiReqMethods[command];
+                method.ResetValues();
+
+                JObject obj = StdJson.JObjectFromString(parameters.JSON);
+                foreach (JProperty p in obj.Properties()) {
+                    if (method.ParameterMap.ContainsKey(p.Name)) {
+                        UiReqPara para = method.ParameterMap[p.Name];
+                        para.Value = p.Value.ToObject(para.Type);
+                    }
+                }
+
+                object[] paramValues = method.Parameters.Select(p => p.Value).ToArray();
+                return await method.TheDelegate(paramValues);
+            }
+
+            return ReqResult.Bad("Unknown command: " + command);
+        }
 
         public virtual Task OnConfigChanged(ObjectRef[] changedObjects) { return Task.FromResult(true); }
 
@@ -43,5 +105,33 @@ namespace Ifak.Fast.Mediator.Dashboard
         public virtual Task OnAlarmOrEvents(AlarmOrEvent[] alarmOrEvents) { return Task.FromResult(true); }
 
         public virtual Task OnConnectionClosed() { return Task.FromResult(true); }
+
+        public class UiReqMethod
+        {
+            public UiReqMethod(UIReqDelegate theDelegate, UiReqPara[] parameters) {
+                TheDelegate = theDelegate;
+                Parameters = parameters;
+                ParameterMap = parameters.ToDictionary(p => p.Name);
+            }
+
+            public UIReqDelegate TheDelegate { get; private set; }
+            public UiReqPara[] Parameters { get; private set; }
+            public Dictionary<string, UiReqPara> ParameterMap { get; private set; }
+
+            public void ResetValues() {
+                foreach (var p in Parameters) {
+                    p.Value = p.DefaultValue;
+                }
+            }
+        }
+
+        public class UiReqPara
+        {
+            public string Name { get; set; }
+            public Type Type { get; set; }
+            public bool HasDefaultValue { get; set; }
+            public object DefaultValue { get; set; }
+            public object Value { get; set; }
+        }
     }
 }
