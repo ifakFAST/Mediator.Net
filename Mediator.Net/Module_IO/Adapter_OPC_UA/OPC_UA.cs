cf4036ee6e805c665b0790ad79e8f91cf1270530
Christian Hübner <christian.huebner@ifak.eu>
2023-12-07 01:05:39 +0100

    IO: Better handling of untrusted client certificate error situation

--- Mediator.Net/Module_IO/Adapter_OPC_UA/OPC_UA.cs
+++ Mediator.Net/Module_IO/Adapter_OPC_UA/OPC_UA.cs
@@ -14,6 +14,7 @@ using Workstation.ServiceModel.Ua;
 using Workstation.ServiceModel.Ua.Channels;
 using Ifak.Fast.Mediator.Util;
 using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace Ifak.Fast.Mediator.IO.Adapter_OPC_UA
 {
@@ -172,7 +173,7 @@ namespace Ifak.Fast.Mediator.IO.Adapter_OPC_UA
                     TimeoutHint = (uint)timeout.TotalMilliseconds,
                 };
 
-                var channel = new ClientSessionChannel(
+                var channel = new MyUaChannel(
                             localDescription: appDescription,
                             certificateStore: certificateStore,
                             userIdentity: identity,
@@ -987,5 +988,59 @@ namespace Ifak.Fast.Mediator.IO.Adapter_OPC_UA
             }
             return false;
         }
+    }
+}
+
+
+// We need this MyUaChannel class in order to early fail when the client certificate is not trusted by the server
+// Otherwise the connection will timeout
+public sealed class MyUaChannel : ClientSessionChannel {
+
+    public MyUaChannel(
+        ApplicationDescription localDescription,
+        ICertificateStore? certificateStore,
+        IUserIdentity userIdentity,
+        EndpointDescription remoteEndpoint,
+        ILoggerFactory? loggerFactory = null,
+        ClientSessionChannelOptions? options = null) :
+
+        base(localDescription, certificateStore, userIdentity, remoteEndpoint, loggerFactory, options) {
+
+    }
+
+    internal delegate void MyEventHandler();
+
+    private event MyEventHandler? FaultedWhileOpening;
+
+    protected override Task OnFaulted(CancellationToken token = default) {
+        FaultedWhileOpening?.Invoke();
+        return base.OnFaulted(token);
+    }
+
+    public async Task OpenAsync() {
+
+        using var cancelOpenAsync = new CancellationTokenSource();
+
+        async Task DelayAbort() {
+            await Task.Delay(1000);
+            cancelOpenAsync.Cancel(); // will throw Exception when disposed; no problem here
+        }
+
+        void OnFaultedWhileOpening() {
+            Task _ = DelayAbort();
+        }
+
+        FaultedWhileOpening += OnFaultedWhileOpening;
+        Task taskOpen = base.OpenAsync(); // this OpenAsync Task may not fail until timeout even if channel is faulted already
+        Task taskFaulted = Task.Delay(Timeout.Infinite, cancelOpenAsync.Token);
+        Task firstCompleted = await Task.WhenAny(taskOpen, taskFaulted);
+        FaultedWhileOpening -= OnFaultedWhileOpening;
+
+        if (firstCompleted == taskFaulted) {
+            string msg = GetPendingException()?.Message ?? "Fault on connect.";
+            throw new Exception(msg);
+        }
+
+        await taskOpen;
     }
 }
