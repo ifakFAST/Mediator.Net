a299ddace62c9c2592746a3f428a50218f5c2fd1
Christian Hübner <christian.huebner@ifak.eu>
2024-07-15 13:51:29 +0200

    MediatorLib: Version info update

--- Mediator.Net/MediatorLib/Util/VersionInfo.cs
+++ Mediator.Net/MediatorLib/Util/VersionInfo.cs
@@ -1,19 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Reflection;
 
-namespace Ifak.Fast.Mediator.Util
+namespace Ifak.Fast.Mediator.Util;
+
+public static class VersionInfo
 {
-    public static class VersionInfo
-    {
-        public static string ifakFAST_Str() {
-            Version v = ifakFAST();
-            if (v == null) return "0";
-            return v.ToString(fieldCount: 3).Trim();
-        }
+    public static string ifakFAST_Str() {
+        var v = ifakFAST();
+        if (v == null) return "0";
+        return v.ToString();
+    }
 
-        public static Version ifakFAST() {
-            return typeof(Timestamp).Assembly.GetName().Version;
-        }
+    public static Version? ifakFAST() {
+
+        Assembly assembly = typeof(Timestamp).Assembly;
+
+        string? versionInfo = assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+            .InformationalVersion;
+
+        bool dev = versionInfo != null && versionInfo.Contains("-");
+        System.Version? version = assembly.GetName().Version;
+
+        if (version == null) return null;
+        
+        return new Version() {
+            Major = version.Major,
+            Minor = version.Minor,
+            Build = version.Build,
+            Dev = dev
+        };
     }
 }
+
+public sealed class Version
+{
+    public int Major { get; set; }
+    public int Minor { get; set; }
+    public int Build { get; set; }
+    public bool Dev { get; set; }
+
+    public override string ToString() {
+        string dev = Dev ? "dev" : "";
+        return $"{Major}.{Minor}.{Build}{dev}";
+    }
+}
+
